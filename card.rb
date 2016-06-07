d4dd7eee0c7afdf733fb66bddf6d9546677d84fa
tallkeith <tallestkeith@gmail.com>
2016-06-07 12:41:24 -0400

    added card file

---
+++ card.rb
@@ -0,0 +1,20 @@
+class Card 
+
+	attr_reader :suit, :value
+
+	def initialize(suit, value)
+		@suit = suit
+		@value = value
+	end
+
+	def value
+		return 10 if ["J", "Q", "K"].include?(@value)
+		return 11 if @value == "A"
+		return @value
+	end
+	
+	def to_s
+		"#{@value}-#{@suit}"
+	end
+
+end
